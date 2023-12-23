b742a8d1d566071af37aa0f1c787429208822b33
Svetlozar <ext-svetlozar@getitdone.co>
2023-12-23 22:34:43 +0700

    Add kenyalaw like theme

--- app/src/app/(app)/provide-theme.tsx
+++ app/src/app/(app)/provide-theme.tsx
@@ -1,7 +1,27 @@
 "use client";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const defaultTheme = createTheme();
+import { ThemeOptions } from "@mui/material/styles";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#932c2b",
+    },
+    secondary: {
+      main: "#faa21c",
+    },
+    error: {
+      main: "#ff3636",
+    },
+    warning: {
+      main: "#e08f4f",
+    },
+  },
+};
+
+const defaultTheme = createTheme(themeOptions);
 
 export default function ProvideTheme({
   children,

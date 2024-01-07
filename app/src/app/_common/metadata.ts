8ec958dc6ee53c44d2c3b1aebcc65b1831a1892a
Svetlozar <ext-svetlozar@getitdone.co>
2024-01-07 17:25:59 +0700

    share metadata between layouts

---
+++ app/src/app/_common/metadata.ts
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Causelist Kenya",
+  description: "Access Kenya Causelist",
+  manifest: "/manifest.webmanifest",
+};
+
+export default metadata;

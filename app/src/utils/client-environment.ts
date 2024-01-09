7ef568885e8e978721efa607caebd3e41ae4dcd0
Svetlozar <ext-svetlozar@getitdone.co>
2024-01-09 13:42:14 +0700

    Fix the evn detection in browser

--- app/src/utils/client-environment.ts
+++ app/src/utils/client-environment.ts
@@ -1,7 +1,9 @@
 export const environment =
-  process.env.NEXT_PUBLIC_SENTRY_STAGING_DOMAIN === window.location.hostname
-    ? "staging"
-    : process.env.NEXT_PUBLIC_SENTRY_PRODUCTION_DOMAIN ===
-      window.location.hostname
-    ? "production"
-    : "development";
+  typeof window !== "undefined"
+    ? process.env.NEXT_PUBLIC_SENTRY_STAGING_DOMAIN === window.location.hostname
+      ? "staging"
+      : process.env.NEXT_PUBLIC_SENTRY_PRODUCTION_DOMAIN ===
+        window.location.hostname
+      ? "production"
+      : "development"
+    : process.env.NEXT_PUBLIC_SENTRY_STAGING_DOMAIN;

874cdffb4afd5748c288d487124445d6ad7a2e01
Svetlozar <ext-svetlozar@getitdone.co>
2024-01-12 10:03:32 +0800

    Adjust crawler run script

--- kenyalaw-crawler/run.sh
+++ kenyalaw-crawler/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
 if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET" -o -z "$S3_REGION"  -o -z "$S3_ENDPOINT" ]; then
   echo "S3_REGION, S3_ENDPOINT, S3_ACCESS_KEY and S3_SECRET must be set"
   exit 1
@@ -25,8 +23,11 @@ if [ "$CRAWLER_TEST" = "job" ]; then
   touch ${STORAGE_DIR}/request_queues/default/dummy.json
   echo "Job test run at $(date -Is)" > ${STORAGE_DIR}/crawler.log.txt
 else
+  echo "Start crawler in ${V}"
   yarn start:${V} ${STORAGE_DIR} > ${STORAGE_DIR}/crawler.log.txt 2>${STORAGE_DIR}/crawler.error.txt
+  echo "Start process data in ${V}"
   yarn process:${V} ${STORAGE_DIR} > ${STORAGE_DIR}/process.log.txt 2>${STORAGE_DIR}/process.error.txt
 fi
 
+echo "Uploading logs..."
 yarn upload-logs:${V} ${STORAGE_DIR}

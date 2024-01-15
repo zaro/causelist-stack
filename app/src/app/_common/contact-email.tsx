c23bc662b11532098f9d467a9505ef71fd42af3f
Svetlozar <ext-svetlozar@getitdone.co>
2024-01-15 08:49:21 +0800

    change support email

--- app/src/app/_common/contact-email.tsx
+++ app/src/app/_common/contact-email.tsx
@@ -3,7 +3,7 @@ import Link from "@mui/material/Link";
 import { useEffect, useState } from "react";
 
 const contactEmail = "c:o:n:t:a:c:t:@:c:a:u:s:e:l:i:s:t:.:c:o:.:k:e";
-const supportEmail = "h:e:l:p:@:c:a:u:s:e:l:i:s:t:.:c:o:.:k:e";
+const supportEmail = "i:n:f:o:@:c:a:u:s:e:l:i:s:t:.:c:o:.:k:e";
 
 export default function ContactEmailLink({ contact }: { contact?: boolean }) {
   const [email, setEmail] = useState("???");

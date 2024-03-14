d5034248b6a75811107ed38a47837c47a9ac851f
Svetlozar <ext-svetlozar@getitdone.co>
2024-03-14 12:19:55 +0800

    Price 1 for testing

--- app/src/api/packages.ts
+++ app/src/api/packages.ts
@@ -1,46 +1,46 @@
 export interface SubscriptionPackage {
   id: string;
   period: number;
-  unit: "day" | "week" | "month" | "year";
+  unit: 'day' | 'week' | 'month' | 'year';
   price: number;
   title: string;
 }
 
 export const TRIAL_PACKAGE: SubscriptionPackage = {
-  id: "trial",
+  id: 'trial',
   period: 1,
-  unit: "month",
+  unit: 'month',
   price: 0,
-  title: "Trial Subscription",
+  title: 'Trial Subscription',
 };
 export const PACKAGES: SubscriptionPackage[] = [
   {
-    id: "week1",
+    id: 'week1',
     period: 1,
-    unit: "week",
+    unit: 'week',
     // price: 100,
     price: 1,
-    title: "1 Week",
+    title: '1 Week',
   },
   {
-    id: "month1",
+    id: 'month1',
     period: 1,
-    unit: "month",
+    unit: 'month',
     price: 350,
-    title: "1 Month",
+    title: '1 Month',
   },
   {
-    id: "month6",
+    id: 'month6',
     period: 6,
-    unit: "month",
+    unit: 'month',
     price: 1950,
-    title: "6 Months",
+    title: '6 Months',
   },
   {
-    id: "year1",
+    id: 'year1',
     period: 1,
-    unit: "year",
+    unit: 'year',
     price: 3850,
-    title: "1 year",
+    title: '1 year',
   },
 ];

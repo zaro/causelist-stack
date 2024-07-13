55df4faca8a2afbd6738ef0eac0c07e89ae21bc0
Svetlozar <ext-svetlozar@getitdone.co>
2024-07-13 18:08:40 +0300

    New Pictures for carousell

--- app/src/app/(app)/_components/ad-carousell.tsx
+++ app/src/app/(app)/_components/ad-carousell.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Button, Box } from "@mui/material";
-import Image1 from "../../carousell/carousell1.jpg";
-import Image2 from "../../carousell/carousell2.jpg";
-import Image3 from "../../carousell/carousell3.jpg";
-import Image4 from "../../carousell/carousell4.jpg";
-import Image5 from "../../carousell/carousell5.jpg";
-import Image6 from "../../carousell/carousell6.jpg";
-import Image7 from "../../carousell/carousell7.jpg";
+import Image1 from "../../carousell/carousell-1.jpg";
+import Image2 from "../../carousell/carousell-2.jpg";
+import Image3 from "../../carousell/carousell-3.jpg";
+import Image4 from "../../carousell/carousell-4.jpg";
+import Image5 from "../../carousell/carousell-5.jpg";
+import Image6 from "../../carousell/carousell-6.jpg";
+import Image7 from "../../carousell/carousell-7.jpg";
+import Image8 from "../../carousell/carousell-8.jpg";
+import Image9 from "../../carousell/carousell-9.jpg";
+import Image10 from "../../carousell/carousell-10.jpg";
+import Image11 from "../../carousell/carousell-11.jpg";
+import Image12 from "../../carousell/carousell-12.jpg";
+import Image13 from "../../carousell/carousell-13.jpg";
+
 import Image, { StaticImageData } from "next/image";
 
 interface CarouselItemProps {
@@ -31,43 +38,67 @@ function CarouselItem(props: CarouselItemProps) {
 const items = [
   {
     img: Image1,
-    description: "Image 1 description",
+    description: "Image description",
   },
   {
     img: Image2,
-    description: "Image 2 description",
+    description: "Image description",
   },
   {
     img: Image3,
-    description: "Image 3 description",
+    description: "Image description",
   },
   {
     img: Image4,
-    description: "Image 3 description",
+    description: "Image description",
   },
   {
     img: Image5,
-    description: "Image 3 description",
+    description: "Image description",
   },
   {
     img: Image6,
-    description: "Image 3 description",
+    description: "Image description",
   },
   {
     img: Image7,
-    description: "Image 3 description",
+    description: "Image description",
+  },
+  {
+    img: Image8,
+    description: "Image description",
+  },
+  {
+    img: Image9,
+    description: "Image description",
+  },
+  {
+    img: Image10,
+    description: "Image description",
+  },
+  {
+    img: Image11,
+    description: "Image description",
+  },
+  {
+    img: Image12,
+    description: "Image description",
+  },
+  {
+    img: Image13,
+    description: "Image description",
   },
 ];
 
 export function AdCarousel() {
-  return <></>;
-  // return (
-  //   <Box sx={{ width: "90%" }}>
-  //     <Carousel indicatorContainerProps={{ style: { paddingTop: "10px" } }}>
-  //       {items.map((item, i) => (
-  //         <CarouselItem key={i} {...item} />
-  //       ))}
-  //     </Carousel>
-  //   </Box>
-  // );
+  // return <></>;
+  return (
+    <Box sx={{ width: "90%" }}>
+      <Carousel indicatorContainerProps={{ style: { paddingTop: "10px" } }}>
+        {items.map((item, i) => (
+          <CarouselItem key={i} {...item} />
+        ))}
+      </Carousel>
+    </Box>
+  );
 }
